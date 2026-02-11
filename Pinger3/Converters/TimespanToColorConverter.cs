f2f3453aca466a7a41118e6a54a39149d30547d8
GalteXx <maximkors@gmail.com>
2026-02-11 15:14:17 +0100

    Added binding to styles, implemented converters

---
+++ Pinger3/Converters/TimespanToColorConverter.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace Pinger3.Converters
+{
+    internal class TimespanToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not TimeSpan timeSpan)
+                throw new ArgumentException("Invalid value or parameter type");
+
+            if (timeSpan == TimeSpan.FromMilliseconds(-1d))
+                return targetType == typeof(Color) ? Colors.Gray
+                    : new SolidColorBrush(Colors.Gray);
+            //i set 100ms a time for the ping to get the "reddest", as I dont want to mess with tuples for now
+            var clamValue = Math.Clamp(timeSpan.TotalMilliseconds / 100, 0, 1);
+
+            byte r, g, b = 0;
+
+            if (clamValue < 0.5)
+            {
+                double t = clamValue / 0.5;
+                r = (byte)(255 * t);
+                g = 255;
+            }
+            else
+            {
+                double t = (clamValue - 0.5) / 0.5;
+                r = 255;
+                g = (byte)(255 * (1 - t));
+            }
+            if (targetType == typeof(Color))
+                return Color.FromRgb(r, g, b);
+            else if (targetType == typeof(Brush))
+                return new SolidColorBrush(Color.FromRgb(r, g, b));
+            else
+                throw new ArgumentException("Invalid target type");
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pinger3/Converters/TimespanToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Pinger3.Converters
+{
+    internal class TimespanToStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not TimeSpan Value)
+                throw new ArgumentException("Invalid value type");
+            if (Value == TimeSpan.MinValue || Value == TimeSpan.FromMilliseconds(-1d))
+                return "N/A";
+            if (Value == TimeSpan.MaxValue)
+                return "Timeout";
+            return $"{double.Round(Value.TotalMilliseconds, 1)} ms";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
