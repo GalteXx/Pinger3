3e27d9933f236193283b12376abe1c42ca3f09bd
GalteXx <maximkors@gmail.com>
2026-02-26 01:51:28 +0100

    Mainwindow UI almost Finished

---
+++ Pinger3/Converters/BoolToToggleStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Pinger3.Converters
+{
+    internal class BoolToToggleStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not bool boolValue || parameter is not string converterParam)
+                return Avalonia.Data.BindingNotification.UnsetValue;
+            var options = converterParam.Split('|');
+            if (options.Length != 2)
+                return Avalonia.Data.BindingNotification.UnsetValue;
+            return boolValue ? options[0] : options[1];
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
