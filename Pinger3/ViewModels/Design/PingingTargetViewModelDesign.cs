21b0a0154be9be5b28cb20e7931d215507d41c6e
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-09-21 05:08:31 +0200

    Design cleanup

--- Pinger3/ViewModels/Design/PingingTargetViewModelDesign.cs
+++ Pinger3/ViewModels/Design/PingingTargetViewModelDesign.cs
@@ -1,48 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.Input;
-using Pinger3.Models;
 using Pinger3.ViewModels.Controls;
 using System;
 using System.ComponentModel;
 
-namespace Pinger3.ViewModels.Design
+namespace Pinger3.ViewModels.Design;
+
+public class PingingTargetViewModelDesign : IPingingTargetViewModel
 {
-    public class PingingTargetViewModelDesign : IPingingTargetViewModel
+    public static PingingTargetViewModelDesign Instance => new();
+
+    public string DomainOrAddress => "192.168.2.1";
+
+    public string Name => "Mockup Server";
+
+    public TimeSpan Ping { get; } = TimeSpan.FromMilliseconds(Random.Shared.Next(1, 120));
+    public TimeSpan TimeSinceLastRequest { get; } = TimeSpan.FromMilliseconds(Random.Shared.Next(1, 100));
+
+    public bool IsActive
     {
-
-        public static PingingTargetViewModelDesign Instance { get => new(); }
-        public PingingTargetViewModelDesign()
-        {
-            Ping = TimeSpan.FromMilliseconds(Random.Shared.Next(1, 120));
-            TimeSinceLastRequest = TimeSpan.FromMilliseconds(Random.Shared.Next(1, 100));
-        }
-
-        public string DomainOrAddress => "192.168.2.1";
-
-        public string Name => "Mockup Server";
-
-        public TimeSpan Ping { get; }
-        public TimeSpan TimeSinceLastRequest { get; }
-
-        public bool IsActive
-        {
-            get
-            {
-                return Random.Shared.Next(0, 2) == 0;
-            }
-            set { }
-        }
-
-        public IRelayCommand StartPingingCommand => throw new NotImplementedException();
-
-        public IRelayCommand StopPingingCommand => throw new NotImplementedException();
-
-        public IRelayCommand TogglePingingCommand => throw new NotImplementedException();
-
-        public TimeSpan DelayBetweenRequests => TimeSpan.FromMilliseconds(Random.Shared.Next(30, ((int)TimeSpan.FromHours(2).TotalMilliseconds)));
-
-        public ConfigValidationErrors ValidationErrors => ConfigValidationErrors.None;
+        get => Random.Shared.Next(0, 2) == 0;
+        set { }
+    }
+    
+    public TimeSpan DelayBetweenRequests =>
+        TimeSpan.FromMilliseconds(Random.Shared.Next(30, (int)TimeSpan.FromHours(2).TotalMilliseconds));
 
 #pragma warning disable CS0067 // The event is never used
-        public event PropertyChangedEventHandler? PropertyChanged;
-    }
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
