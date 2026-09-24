9cd2befcf1ea52e99ce0f27cabb81d651fb6cce1
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-09-24 11:52:46 +0200

    Added timer to update TimeSinceLastRequest for Endpoint ViewModels

--- Pinger3/ViewModels/PageViewModels/MainWindowViewModel.cs
+++ Pinger3/ViewModels/PageViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
 using Pinger3.Services;
 using Pinger3.ViewModels.Controls;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Avalonia.Threading;
 using Pinger3.Services.Pinging;
 using Pinger3.ViewModels.Controls.Factories;
 
@@ -15,8 +17,10 @@ namespace Pinger3.ViewModels.PageViewModels
         // TODO: get an observable Dictionary
         public ObservableCollection<IPingingTargetViewModel> Endpoints { get; } = [];
 
+        private readonly TimeSpan _timerIntervalMs = TimeSpan.FromMilliseconds(200);
         private readonly IEndpointRepository _repository;
         private readonly IPingTargetViewModelFactory _factory;
+        private readonly DispatcherTimer _timespanUpdateTimer;
         private readonly IPingManager _pingManager;
 
         public MainWindowViewModel(IEndpointRepository endpointRepository, IPingTargetViewModelFactory factory,
@@ -26,6 +30,13 @@ namespace Pinger3.ViewModels.PageViewModels
             _factory = factory;
             _pingManager = pingManager;
             _repository.EndpointUpdated += UpdateViewModel;
+            _timespanUpdateTimer = new DispatcherTimer(DispatcherPriority.Default)
+            {
+                Interval = _timerIntervalMs,
+                IsEnabled = true
+            };
+            _timespanUpdateTimer.Tick += UpdateEnpointViewModels;
+            _timespanUpdateTimer.Start();
 
             foreach (var endpoint in _repository.CachedEndpoints.Values)
             {
@@ -40,6 +51,14 @@ namespace Pinger3.ViewModels.PageViewModels
             {
                 Endpoints.FirstOrDefault(vm => vm.Id == update.Id)?.OnPingReceived(update);
             };
+        }
+
+        private void UpdateEnpointViewModels(object? sender, EventArgs e)
+        {
+            foreach (var endpoint in Endpoints)
+            {
+                endpoint.UpdateTimeSinceLastRequest(_timerIntervalMs);
+            }
         }
 
         private void UpdateViewModel(object? sender, string e)
