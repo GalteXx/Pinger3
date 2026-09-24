c560d54918b00177ce0f87c6eda02aee9182c040
Korsakov Maksim <65533265+GalteXx@users.noreply.github.com>
2026-09-24 11:40:29 +0200

    Trimmed SettingsWindowViewModel.cs

--- Pinger3/ViewModels/PageViewModels/ISettingsViewModel.cs
+++ Pinger3/ViewModels/PageViewModels/ISettingsViewModel.cs
@@ -1,11 +1,7 @@
-﻿using Pinger3.ViewModels.Controls;
-using System.Collections.ObjectModel;
-
-namespace Pinger3.ViewModels.PageViewModels
+﻿namespace Pinger3.ViewModels.PageViewModels
 {
     public interface ISettingsViewModel
     {
         IPopoutWindowViewModel PopoutVM { get; }
-        ObservableCollection<IPingingTargetWrapperViewModel> SelectableForPopoutTargetVMs { get; }
     }
 }
--- Pinger3/ViewModels/PageViewModels/SettingsWindowViewModel.cs
+++ Pinger3/ViewModels/PageViewModels/SettingsWindowViewModel.cs
@@ -1,41 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using Pinger3.Services;
-using Pinger3.ViewModels.Controls;
-using System;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 
 namespace Pinger3.ViewModels.PageViewModels
 {
-    public partial class SettingsViewModel : ObservableObject, ISettingsViewModel, INotifyPropertyChanged
+    public partial class SettingsViewModel(IPopoutWindowViewModel popoutVm)
+        : ObservableObject, ISettingsViewModel, INotifyPropertyChanged
     {
-        private readonly ParsedTargetsViewModelsBuilder _targetsVmBuilder;
-
-        [ObservableProperty]
-        private IPopoutWindowViewModel _popoutVM;
-
-        public ObservableCollection<IPingingTargetWrapperViewModel> SelectableForPopoutTargetVMs { get; private set; }
-
-        public SettingsViewModel(IPopoutWindowViewModel popoutVM, ParsedTargetsViewModelsBuilder targetsVMBuilder)
-        {
-            _popoutVM = popoutVM;
-            _targetsVmBuilder = targetsVMBuilder;
-            _targetsVmBuilder.ViewModelsBuilt += OnTargetVMsRebuilt;
-            SelectableForPopoutTargetVMs = [];
-        }
-
-        private void WrapViewModels()
-        {
-            SelectableForPopoutTargetVMs = [.. _targetsVmBuilder.PingingTargetsByCategory(PingingTargetCategory.ValidTargets)
-                .Select(x => new PingingTargetWrapperViewModel(x, PopoutVM))];
-
-        }
-
-        private void OnTargetVMsRebuilt(object? sender, EventArgs e)
-        {
-            WrapViewModels();
-            OnPropertyChanged(nameof(SelectableForPopoutTargetVMs));
-        }
+        [ObservableProperty] private IPopoutWindowViewModel _popoutVM = popoutVm;
     }
 }
